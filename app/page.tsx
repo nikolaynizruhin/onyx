dd37f80cea92f8abcce8146f98fd6f2e08655bdf
Mykola Nizriukhin <lgnexus4fun@gmail.com>
2024-06-22 15:00:37 +0200

    Update layout

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen justify-center items-stretch sm:items-center px-6 py-24 md:p-24 bg-[#111D28]">
+    <main className="flex min-h-screen justify-center items-stretch sm:items-center px-6 py-32 md:p-24 bg-[#111D28]">
       <div className="flex flex-col items-center justify-between">
         <Image className="sm:mb-36 w-52 md:w-72 lg:w-auto" src="/logo.svg" alt="Onyx Logo" width={371} height={123} />
         <Image className="sm:mb-36 w-[450px] md:w-[550px] lg:w-auto" src="/under-construction.svg" alt="Under Constraction" width={663} height={121} />
